ef2378a519085c16fd43c690f7bc596143f798c0
Kelvin <kmunyimbili@gmail.com>
2023-09-07 22:01:50 +0200

    ADD FINISHED LOGIC AND VALIDATIONS
    This is the complete application. Fully functional.

--- mainjs.js
+++ mainjs.js
@@ -7,9 +7,10 @@ const newLine = document.createElement("br");
 bottomDisplay.value = "0";
 midDisplay.value = "";
 topDisplay.value = "";
-let operator = "";
-let firstNum = "";
-let secondNum = "";
+const operators = ["+", "-", "*", "/"];
+let operator = "+";
+let firstNum = 0;
+let secondNum = 0;
 let result = 0;
 
 buttonArea.addEventListener('click', button => {
@@ -17,89 +18,95 @@ buttonArea.addEventListener('click', button => {
     if (button.target.nodeName == "BUTTON") {
 
         if (button.target.innerText == "=") {
-            if (firstNum && secondNum) {
+            if (!isNaN(firstNum)) {
+                secondNum = parseFloat(bottomDisplay.value);
                 evaluate(firstNum, secondNum);
-                return;
-            } else {
-                if (parseInt(bottomDisplay.value)) {
-                    firstNum = parseInt(bottomDisplay.value);
-                    topDisplay.value = bottomDisplay.value;
-                    return;
-                } else {
-                    if (firstNum && !secondNum) {
-                        topDisplay.value = firstNum;
-                        return;
-                    } else {
-                        return;
-                    }                        
-                }
-            }
-        } else {
-
-            if (button.target.innerText == "c") {
-                bottomDisplay.value = "0";
-                midDisplay.value = "";
-                topDisplay.value = "";
+                midDisplay.value += secondNum;
+                bottomDisplay.value = firstNum;
                 return;
             }
-
-            if (button.target.innerText == "<") {
-                if (bottomDisplay.value == "0") {
-                    return
-                } else {
-                    bottomDisplay.value = bottomDisplay.value.slice(0, -1);
-                    if (bottomDisplay.value == "") {
-                        bottomDisplay.value = "0";
-                    }
-                    return;
-                }
-            }
-            
-            if (button.target.parentNode.className == "operationKeys keys" && !(button.target.innerText == "=")) {
-                firstNum = parseInt(bottomDisplay.value);
-                operator = button.target.innerText;
-                midDisplay.value = bottomDisplay.value + operator;
-                bottomDisplay.value = "0";
-                topDisplay.value = firstNum;
+            if (parseFloat(bottomDisplay.value)) {
+                firstNum = parseFloat(bottomDisplay.value);
+                topDisplay.value = bottomDisplay.value;
                 return;
             }
+            return;
+        }
 
+        if (button.target.innerText == "c") {
+            bottomDisplay.value = "0";
+            midDisplay.value = "";
+            topDisplay.value = "";
+            firstNum = 0;
+            return;
+        }
+
+        if (button.target.innerText == "<") {
             if (bottomDisplay.value == "0") {
-                bottomDisplay.value = button.target.innerText;
+                return
             } else {
-                bottomDisplay.value += button.target.innerText;
+                bottomDisplay.value = bottomDisplay.value.slice(0, -1);
+                if (bottomDisplay.value == "") {
+                    bottomDisplay.value = "0";
+                }
+                return;
             }
         }
-    }
-});
+        
+        if (button.target.parentNode.className == "operationKeys keys" && !(button.target.innerText == "=")) {
+            if (checkForOperator() && operators.includes(button.target.innerText)) {
+                midDisplay.value = midDisplay.value.slice(0, -2) + button.target.innerText + " ";
+                operator = button.target.innerText;
+                return;
+            }
 
-function clear(){
-    firstNum = NaN;
-    secondNum = NaN;
-    mid.value = "";
-    resultField.value = "0";
+            firstNum = parseFloat(bottomDisplay.value);
+            operator = button.target.innerText;
+            midDisplay.value = " = " + bottomDisplay.value + " " + operator + " ";
+            bottomDisplay.value = "0";
+            topDisplay.value = firstNum;
+            return;
+        }
+        if (bottomDisplay.value == "0") {
+            bottomDisplay.value = button.target.innerText;
+        } else {
+            if (bottomDisplay.value.includes(".") && button.target.innerText == ".") return;
+
+            bottomDisplay.value += button.target.innerText;
+        }
+
+    }
+})
+
+function checkForOperator(){
+    const stringLength = midDisplay.value.length;
+    const operator = midDisplay.value.charAt(stringLength - 2);
+    const operators = ["+", "-", "*", "/"];
+    if (operators.includes(operator)) {
+        return true;
+    }
 }
 
 function evaluate(a, b) {
     switch (operator) {
         case "+":
-            result = a + b;
-            topDisplay.value = result;
+            firstNum = a + b;
+            topDisplay.value = firstNum.toFixed(8);
             break;
 
         case "-":
-            result = a - b;
-            topDisplay.value = result;
+            firstNum = a - b;
+            topDisplay.value = firstNum.toFixed(8);
             break;
 
         case "/":
-            result = a / b;
-            topDisplay.value = result;
+            firstNum = a / b;
+            topDisplay.value = firstNum.toFixed(8);
             break;
     
         case "*":
-            result = a * b;
-            topDisplay.value = result;
+            firstNum = a * b;
+            topDisplay.value = firstNum.toFixed(8);
             break;
 
         default:
